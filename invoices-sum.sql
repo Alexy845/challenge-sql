cba31b8440d536cd68bba6cd6aea3ed9ad6f4fb3
Alexy845 <alexy.daubresse@ynov.com>
2023-10-23 16:19:09 +0200

    Quête 2

---
+++ invoices-sum.sql
@@ -0,0 +1,7 @@
+SELECT SUM(invoices.Total) AS AllInvoicesTotalPrice
+FROM
+    customers
+JOIN
+    invoices ON customers.CustomerId = invoices.CustomerId
+WHERE
+    customers.FirstName = 'Tim' AND customers.LastName = 'Goyer';
