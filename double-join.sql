dea534ffc7cc7e4c6b609b28a792cf9936fe5d20
Alexy845 <alexy.daubresse@ynov.com>
2023-10-23 15:23:12 +0200

    ADD

--- double-join.sql
+++ double-join.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT tracks.Name AS TrackName, playlists.Name AS PlayListName
+SELECT DISTINCT tracks.Name AS TrackName, playlists.Name AS PlaylistName
 FROM playlist_track
 INNER JOIN playlists ON playlist_track.PlaylistId = playlists.PlaylistId
 INNER JOIN tracks ON playlist_track.TrackId = tracks.TrackId
