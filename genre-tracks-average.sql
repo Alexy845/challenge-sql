fc23a01d665e3cb82ddc3301d493bf17236372b0
Alexy845 <alexy.daubresse@ynov.com>
2023-10-24 10:44:49 +0200

    ADD

---
+++ genre-tracks-average.sql
@@ -0,0 +1,10 @@
+SELECT
+    genres.Name,
+    AVG(tracks.Milliseconds) AS AverageDuration
+FROM
+    genres
+INNER JOIN
+        tracks ON genres.GenreId = tracks.GenreId
+GROUP BY genres.Name
+ORDER BY
+    AverageDuration DESC
