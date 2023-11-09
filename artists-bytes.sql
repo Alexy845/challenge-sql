932ce1189d22926832ed9f6b754448d92a88e035
Alexy845 <alexy.daubresse@ynov.com>
2023-11-09 09:41:29 +0100

    ADD

---
+++ artists-bytes.sql
@@ -0,0 +1,13 @@
+SELECT
+    artists.Name AS ArtistName,
+    albums.Title AS AlbumName,
+    tracks.Name AS TrackName,
+    ROUND(tracks.Bytes / 1048576, 2) || ' MB' AS MegaBytes
+FROM
+    artists
+INNER JOIN
+    albums  ON artists.ArtistId = albums.ArtistId
+INNER JOIN
+    tracks ON albums.AlbumId = tracks.AlbumId
+WHERE
+    albums.Title = 'American Idiot';
